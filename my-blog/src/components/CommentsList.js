a4c689df88035fa13f9dae6c89c5f63a1a52171d
Jayashri <jayashri1394@gmail.com>
2020-05-22 01:58:57 +0200

    front and backend connected

---
+++ my-blog/src/components/CommentsList.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const CommentsList = ({comments}) =>(
+    <>
+            <h3>Comments</h3>
+    {comments.map((comments,key) =>(
+        <div className={"comment"} key={key}>
+                <h4>{comments.username}</h4>
+                <p>{comments.text}</p>
+        </div>
+        ))}
+        </>
+)
+
+export default CommentsList;
--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import articleContent from "./article-content";
 import ArticleList from "../components/ArticleList";
+import CommentsList from "../components/CommentsList";
+import UpvotesSection from "../components/UpvotesSection";
+import AddCommentForm from "../components/AddCommentForm";
 import NotFoundPage from "./NotFoundPage";
+
 const ArticlePage = ({match}) =>{
     const name = match.params.name;
     const article = articleContent.find(article => article.name === name);
+    const [articleInfo,setArticleInfo] = useState({upvotes:0,comments:[]});
+
+    useEffect(()=>{
+        const fetchData = async () => {
+            const result = await fetch(`/api/articles/${name}`)
+            const body = await result.json();
+            setArticleInfo(body);
+        }
+        fetchData();
+    },[name]);
+
     if(!article) return <NotFoundPage />
     const otherArticles = articleContent.filter(article => article.name !== name);
     return(
         <>
             <h1>{article.title}</h1>
+            <UpvotesSection articleName={name} upvotes={articleInfo.upvotes} setArticleInfo={setArticleInfo}/>
             {article.content.map((paragraph,key) =>(
                 <p key = {key}>{paragraph}</p>
             ))}
+            <CommentsList comments = {articleInfo.comments}/>
+            <AddCommentForm articleName={name} setArticleInfo={setArticleInfo}/>
             <h3>other articles:</h3>
             <ArticleList article={otherArticles}/>
         </>
